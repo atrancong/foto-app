4f3e3091df82d1c3f0cb083744b4c82d844ef168
ailien <a.trancong@gmail.com>
2016-12-12 14:14:38 +1100

    tidy flask app

--- app.py
+++ app.py
@@ -3,30 +3,20 @@ from flask import Flask, request, redirect, url_for, render_template, send_from_
 from werkzeug.utils import secure_filename
 from io import BytesIO
 
-UPLOAD_FOLDER = 'uploads/'
-
 app = Flask(__name__)
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods = ['GET','POST'])
 def upload_file():
 	if request.method == 'POST':
 		 file = request.files['file']
 		 if file.filename == '':
-		 	return redirect(request.url) #loop back if no file
+		 	return redirect(request.url) #loop back to front page if no file
 		 if file:
-		 	#filename = secure_filename(file.filename)
-		 	#file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		 	#return redirect(url_for('uploaded_file', filename=filename))
 		 	byte_io = BytesIO()
-		 	file.save(byte_io)
+		 	file.save(byte_io) #save file contents to memory
 		 	byte_io.seek(0)
 		 	return send_file(byte_io, mimetype='image')
 	return render_template('upload.html')
-
-@app.route('/<filename>')
-def uploaded_file(filename):
-	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__=='__main__':
 	app.run(debug=True)
