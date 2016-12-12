515efd21d713c7e9fb4442af476f2a86eb9b2523
ailien <a.trancong@gmail.com>
2016-12-12 14:30:12 +1100

    update app test

--- test_app.py
+++ test_app.py
@@ -7,19 +7,19 @@ from io import BytesIO
 class appTestCase(unittest.TestCase):
 
     def setUp(self):
-        #self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
         app.app.config['TESTING'] = True
         self.app = app.app.test_client()
-        #flaskr.init_db()
 
     def tearDown(self):
     	pass
-        #os.close(self.db_fd)
-        #os.unlink(flaskr.app.config['DATABASE'])
 
     def test_upload(self):
+        """
+        Test: file upload correctly returns uploaded file.
+        
+        Compares uploaded file contents sent in POST request to response contents.
+        """
     	rv = self.app.post('/', data = {'file': (BytesIO('picture file contents'), 'testfile.txt')} )
-    	#print rv.data
     	assert 'picture file contents' in rv.data 
 
 if __name__ == "__main__":
